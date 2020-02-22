4072be95d32c9b1051882c2d96094ba2705f42da
Konstantin Balanchuk <konstantinbalanchuk@gmail.com>
2020-02-22 20:34:14 +0200

    First commit

---
+++ .next/static/webpack/static/development/pages/index.js.78e3e317c6c8f96627ee.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static\\development\\pages\\index.js",[
+/* 0 */,
+/* 1 */
+/*!***************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=D%3A%5Cnext-app%5Cpages%5Cindex.js ***!
+  \***************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=D%3A%5Cnext-app%5Cpages%5Cindex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=D%3A%5Cnext-app%5Cpages%5Cindex.js!./");
+
+
+/***/ }),
+/* 2 */
+false
+])
+//# sourceMappingURL=index.js.78e3e317c6c8f96627ee.hot-update.js.map
---
+++ .next/static/webpack/static/development/pages/index.js.e2019eaa5dc203272dd0.hot-update.js
@@ -0,0 +1,81 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Prices.js":
+/*!******************************!*\
+  !*** ./components/Prices.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var _classCallCheck = __webpack_require__(/*! @babel/runtime-corejs2/helpers/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/classCallCheck.js");
+
+var _possibleConstructorReturn = __webpack_require__(/*! @babel/runtime-corejs2/helpers/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/possibleConstructorReturn.js");
+
+var _getPrototypeOf = __webpack_require__(/*! @babel/runtime-corejs2/helpers/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/getPrototypeOf.js");
+
+var _assertThisInitialized = __webpack_require__(/*! @babel/runtime-corejs2/helpers/assertThisInitialized */ "./node_modules/@babel/runtime-corejs2/helpers/assertThisInitialized.js");
+
+var _inherits = __webpack_require__(/*! @babel/runtime-corejs2/helpers/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/inherits.js");
+
+var _defineProperty = __webpack_require__(/*! @babel/runtime-corejs2/helpers/defineProperty */ "./node_modules/@babel/runtime-corejs2/helpers/defineProperty.js");
+
+var Prices =
+/*#__PURE__*/
+function (_React$Component) {
+  "use strict";
+
+  _inherits(Prices, _React$Component);
+
+  function Prices() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, Prices);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Prices)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "stat", void 0);
+
+    return _this;
+  }
+
+  return Prices;
+}(React.Component);
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/defineProperty.js":
+/*!***********************************************************************!*\
+  !*** ./node_modules/@babel/runtime-corejs2/helpers/defineProperty.js ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var _Object$defineProperty = __webpack_require__(/*! ../core-js/object/define-property */ "./node_modules/@babel/runtime-corejs2/core-js/object/define-property.js");
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    _Object$defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+module.exports = _defineProperty;
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.e2019eaa5dc203272dd0.hot-update.js.map
